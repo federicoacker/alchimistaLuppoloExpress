4e427d2cdd19da8dcf642fc3bc0a48b7aa4d5531
federicoacker <acker.federico@gmail.com>
2026-06-19 13:45:17 +0200

    Fatto index e show per i products, creato funzioni di supporto e middleware

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,6 @@
+import { selectAllProducts } from "../db/queries/selectAllProducts.js";
+import { selectProductBySlug } from "../db/queries/selectProductBySlug.js";
+
 const productController = {
     index,
     show,
@@ -6,12 +9,43 @@ const productController = {
     destroy
 }
 
-function index(request, response) {
-
+async function index(request, response) {
+    const {result : products, error} = await selectAllProducts();
+    switch(error){
+        case 404:
+            return response.status(404).json({
+                error:"Non sono stati trovati prodotti",
+                result:null
+            });
+            break;
+        case 500:
+            return response.status(500).json({
+                error:"C'è stato un errore nel recuperare i dati dei prodotti dal db",
+                result:null
+            }); 
+            break;
+        default:
+            break;
+    }
+    return response.json({
+        error:null,
+        result:products
+    });
 }
 
-function show(request, response) {
+async function show(request, response) {
+    const {result: product, error} = await selectProductBySlug(request.productSlug);
 
+    if(error === 500){
+        return response.status(500).json({
+            error:`C'è stato un errore nel recuperare i dati del prodotto ${request.validatedSlug}`,
+            result:null
+        });
+    }
+    return response.json({
+        error:null,
+        result:product[0]
+    });
 }
 
 function store(request, response) {
--- routers/products.js
+++ routers/products.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import express from "express";
-import productController from "../controllers/products";
+import productController from "../controllers/products.js";
+import { checkProductSlugExists } from "../middlewares/checkProductSlugExists.js";
 
 const productRouter = express.Router();
 
 productRouter.get("/", productController.index);
 
-productRouter.get("/:productSlug", productController.show);
+productRouter.get("/:productSlug", [checkProductSlugExists,productController.show]);
 
 productRouter.post("/", productController.store);
 
