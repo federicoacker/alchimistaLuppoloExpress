1de960b2c48c25dabe0535a239a9b9ded17e746e
federicoacker <acker.federico@gmail.com>
2026-06-20 11:50:11 +0200

    Fixato bug con la checkSlugInDB

--- db/queries/checkSlugInDB.js
+++ db/queries/checkSlugInDB.js
@@ -2,21 +2,20 @@ import connection from "../db.js";
 
 export async function checkSlugInDB(slug, table){
     const query = `
-    SELECT slug 
-        FROM ? 
+    SELECT slug, name 
+        FROM ${table} 
         WHERE slug = ? 
         LIMIT 1;
     `;
     try{
-        console.log(table, slug);
-        const [product] = await connection.execute(query, [table, slug]);
+        const [product] = await connection.execute(query, [slug]);
         if(product.length === 0){
             return {error:404, result:null};
         }
         return {error:null, result:product[0]};
     }
     catch (error){
-        console.log(error);
         return {error:500, result:null};
     }
 }
+
